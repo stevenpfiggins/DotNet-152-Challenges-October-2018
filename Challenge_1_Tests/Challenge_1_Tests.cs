7b7f9af7c26694bc9d12c4ec9f890cb1bf6f9103
stevenpfiggins <sfiggins43@gmail.com>
2018-11-19 10:50:25 -0500

    Unit Testing challenges 1 and 2

---
+++ Challenge_1_Tests/Challenge_1_Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Challenge_1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Challenge_1_Tests
+{
+    [TestClass]
+    public class Challenge_1_Tests
+    {
+        MenuRepository _menuRepo = new MenuRepository();
+        Menu menu;
+        List<Menu> list;
+        List<string> listOfStrings = new List<string>() { "beef", "buns" };
+
+        [TestMethod]
+        public void MenuRepository_AddMenuItemToList_ShouldIncreaseCountByOne()
+        {
+            //Arrange
+            menu = new Menu(5, "Hamburger", "Burger between buns", listOfStrings, 5.99m);
+            list = _menuRepo.GetMenuList();
+            _menuRepo.AddMenuItemToList(menu);
+
+            //Act
+            var actual = list.Count;
+            var expected = 1;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Challenge_2_Tests/Challenge_2_Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Challenge_2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Challenge_2_Tests
+{
+    [TestClass]
+    public class Challenge_2_Tests
+    {
+        ClaimRepository _claimRepo = new ClaimRepository();
+        Claim claim;
+        Queue<Claim> claims;
+        [TestMethod]
+        public void ClaimRepository_AddClaimToQueue_ShouldIncreaseCountByOne()
+        {
+            //Arrange
+            claim = new Claim(1, "house", "fire", 500m, new DateTime(2018, 5, 25), new DateTime(2018, 5, 30), true);
+            claims = _claimRepo.SeeClaimQueue();
+            _claimRepo.AddClaimToQueue(claim);
+
+            //Act
+            var actual = claims.Count;
+            var expected = 1;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
